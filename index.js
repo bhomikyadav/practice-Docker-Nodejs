4896dc2cca3f938e5a4857f3b88339285f987ab2
bhomikyadav <bhomickyadav786@gmail.com>
2023-09-21 21:48:52 +0530

    adding Docker

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5500;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to nodejs with Docker");
+  res.send("Welcome to nodejs with Docker : Example :  Bhomik ");
 });
 
 app.listen(PORT, () => {
